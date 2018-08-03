51a89df77c4e9a9bcfbeda4c3545874fa0cc4fe1
ryanlid <ryanlid@live.com>
2018-08-03 11:21:03 +0800

    copy ch03 basic structure to ch04

--- ch04/App.js
+++ ch04/App.js
@@ -7,7 +7,19 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text, View,
+  TextInput,
+  Button,
+  ScrollView,
+  Dimensions,
+  ListView,
+  Alert,
+  TouchableHighlight,
+  StatusBar
+} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,34 +28,158 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const ds = new ListView.DataSource({
+  rowHasChanged:(r1,r2,)=> (r1!==r2)
+})
+
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(Props){
+    super(Props);
+    this.state = {
+      currentPage:0,
+      dataSource:ds.cloneWithRows([
+        "商品1",
+        "商品2",
+        "商品3",
+        "商品4",
+        "商品5",
+        "商品6",
+        "商品7",
+        "商品8",
+        "商品9",
+        "商品10"
+      ])
+    };
+  }
   render() {
     return (
+      // <View style={styles.container}>
+      //   <Text style={styles.welcome}>Welcome to React Native!</Text>
+      //   <Text style={styles.instructions}>To get started, edit App.js</Text>
+      //   <Text style={styles.instructions}>{instructions}</Text>
+      // </View>
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <StatusBar backgroundColor={'blue'}
+          barStyle={'default'}
+          networkActivityIndicatorVisible={true}> // 显示正在请求网络的状态
+        </StatusBar>
+
+         <View style={styles.searchbar}>
+            <TextInput style={styles.input} placeholder='搜索商品'>
+            </TextInput>
+            <Button style={styles.button} title='搜索' onPress={()=>Alert.alert('你点击了搜索按钮')}></Button>
+        </View>
+        <View style={styles.advertisement}>
+          <ScrollView ref="scrollView"
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled={true}
+            >
+            <TouchableHighlight onPress={()=>Alert.alert('你点击了轮播图',null,null)}>
+              <Text style={{
+                width: Dimensions.get('window').width,
+                height:180,
+                backgroundColor:'gray'
+              }}>
+                广告1
+              </Text>
+              </TouchableHighlight>
+              <TouchableHighlight onPress={()=>Alert.alert('你点击了轮播图',null,null)}>
+            <Text style={{
+              width: Dimensions.get('window').width,
+              height:180,
+              backgroundColor:'orange'
+            }}>
+              广告2
+            </Text>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={()=>Alert.alert('你点击了轮播图',null,null)}>
+            <Text style={{
+              width: Dimensions.get('window').width,
+              height:180,
+              backgroundColor:'yellow'
+            }}>
+              广告3
+            </Text>
+            </TouchableHighlight>
+          </ScrollView>
+        </View>
+        <View style={styles.products}>
+          {/* <Text>
+            商品列表
+          </Text> */}
+          <ListView dataSource={this.state.dataSource} renderRow={this._renderRow}>
+
+          </ListView>
+        </View>
+
       </View>
     );
+  }
+  componentDidMount(){
+    this._startTimer();
+  }
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
+  _startTimer(){
+    this.interval = setInterval(()=>{
+      nextPage = this.state.currentPage + 1;
+      if(nextPage>=3){
+        nextPage=0;
+      }
+      this.setState({currentPage:nextPage})
+
+      // 计算 scrollView 滚动的X轴偏移量
+      const offSetX = nextPage * Dimensions.get('window').width;
+      this.refs.scrollView.scrollResponderScrollTo({x:offSetX,y:0,animated:true});
+    },2000);
+  }
+  _renderRow = (rowData,sectionID,rowID)=>{
+    return(
+      <View style={styles.row}>
+        <TouchableHighlight onPress={()=>Alert.alert('你点击了商品列表',null,null)}>
+          <Text>
+            {rowData}
+          </Text>
+        </TouchableHighlight>
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  searchbar:{
+    marginTop: Platform.OS === "ios"
+             ? 20
+             : 0,
+    height:40,
+    flexDirection:'row'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  advertisement:{
+    height: 180,
   },
+  products:{
+    flex:1,
+    backgroundColor:'blue',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  input:{
+    flex: 1,
+    borderColor:'gray',
+    borderWidth: 2
+  },
+  button:{
+    flex:1
+  },
+  row:{
+    height:60,
+    justifyContent:'center',
+    alignItems:'center'
+  }
 });
